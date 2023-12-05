f01ebab50f74b29ccdb1be33c0b6c4094be1348e
Kvakino <mikeknor2503@gmail.com>
2023-12-05 01:05:36 +0100

    added parameter functionality from terminal

--- src/main.rs
+++ src/main.rs
@@ -5,53 +5,71 @@ mod day02;
 mod day03;
 mod day04;
 
-use std::fmt::Error;
 use std::time::Instant;
 use std::env;
 
-fn main() -> std::io::Result<()> {
-    let now = Instant::now();
+use colored::Colorize;
 
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    // Check if at least two arguments are provided
-    if args.len() < 2 {
-        println!("Usage: {} <function>", args[0]);
-        Err("2 arguments required");
-    }
-
-    // Create a HashMap to map function names to functions
-    let mut function_map = HashMap::new();
+    let mut function_map: HashMap<&str, fn(bool)> = HashMap::new();
     
-    function_map.insert("--day01 --part1", day01::part1::main as fn(bool));
-    function_map.insert("--day01 --part2", day01::part2::main as fn(bool));
-    function_map.insert("--day02 --part1", day02::part1::main as fn(bool));
-    function_map.insert("--day02 --part2", day02::part2::main as fn(bool));
-    function_map.insert("--day03 --part1", day03::part1::main as fn(bool));
-    function_map.insert("--day03 --part2", day03::part2::main as fn(bool));
-    function_map.insert("--day04 --part1", day04::part1::main as fn(bool));
-    function_map.insert("--day04 --part2", day04::part2::main as fn(bool));
+    function_map.insert("day01 part1", day01::part1::main as fn(bool));
+    function_map.insert("day01 part2", day01::part2::main as fn(bool));
 
-    // Match the argument to get the corresponding function
-    if let Some(&func) = function_map.get(args[1].as_str()) {
-        // Pass "123" as the parameter to the function
-        func("123");
-    } else {
-        println!("Unknown function: {}", args[1]);
+    function_map.insert("day02 part1", day02::part1::main as fn(bool));
+    function_map.insert("day02 part2", day02::part2::main as fn(bool));
+
+    function_map.insert("day03 part1", day03::part1::main as fn(bool));
+    function_map.insert("day03 part2", day03::part2::main as fn(bool));
+
+    function_map.insert("day04 part1", day04::part1::main as fn(bool));
+    function_map.insert("day04 part2", day04::part2::main as fn(bool));
+
+    if args.len() == 1 {
+        let mut sorted_keys: Vec<&&str> = function_map.keys().collect();
+        sorted_keys.sort();
+
+        for key in sorted_keys {
+            let now = Instant::now();
+
+            print!("{}: ", key);
+            function_map[key](false);
+
+            println!("{}: {}", "Elapsed".green().bold(), format!("{:.2?}", now.elapsed()).underline());
+        }
+
+        return Ok(());
     }
     
-        let _ = day01::part1::main();
-        let _ = day01::part2::main();
-        let _ = day02::part1::main();
-        let _ = day02::part2::main();
-        let _ = day03::part1::main();
-        let _ = day03::part2::main();
-        let _ = day04::part1::main();
-        
-    day04::part2::main();
+    if args.len() < 3 {
+        panic!("ERROR: 2 arguments required")
+    }
+    
+    let call_function: &str = &format!("{} {}", args[1], args[2]);
 
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    if let Some(&func) = function_map.get(&call_function) {
+        let now = Instant::now();
 
+        print!("{}: ", call_function);
+        func(args.len() > 3);
+
+        println!("{}: {}", "Elapsed".green().bold(), format!("{:.2?}", now.elapsed()).underline());
+    }
+    
     Ok(())
 }
+
+
+// fn main() {
+//     println!(
+//         "{}, {}, {}, {}, {}, {}, and some normal text.",
+//         "Bold".bold(),
+//         "Red".red(),
+//         "Yellow".yellow(),
+//         "Green Strikethrough".green().strikethrough(),
+//         "Blue Underline".blue().underline(),
+//         "Purple Italics".purple().italic()
+//     );
+// }
