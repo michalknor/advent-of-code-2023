d837b9eb5eaeea2dbee319524ba513dd7947eae9
Kvakino <mikeknor2503@gmail.com>
2024-01-07 01:18:34 +0100

    fixed file

--- src/day04/part2.rs
+++ src/day04/part2.rs
@@ -23,7 +23,11 @@ fn evaluate_ticket(winning_numbers: Vec<u32>, your_numbers: Vec<u32>) -> u32 {
 
 pub fn main(filename: &str) -> String {
     let mut file: File = File::open(filename).expect("Failed to open file");
+<<<<<<< HEAD
 	let mut file_content: String = String::new();
+=======
+	let mut file: File_content: String = String::new();
+>>>>>>> 84da3640cab7e9c156fa7aa9bc01b57a9bcc4c39
 
 
 	file.read_to_string(&mut file_content).expect("Failed to read file content");
--- src/day06/part1.rs
+++ src/day06/part1.rs
@@ -28,7 +28,11 @@ fn number_of_ways_multiplied_together(times: Vec<u32>, distances: Vec<u32>) -> u
 
 pub fn main(filename: &str) -> String {
     let mut file: File = File::open(filename).expect("Failed to open file");
+<<<<<<< HEAD
 	let mut file_content: String = String::new();
+=======
+	let mut file: File_content: String = String::new();
+>>>>>>> 84da3640cab7e9c156fa7aa9bc01b57a9bcc4c39
 
 
 	file.read_to_string(&mut file_content).expect("Failed to read file content");
