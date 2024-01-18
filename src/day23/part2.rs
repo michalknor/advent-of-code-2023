c897dbafa81ea11ef1a37f5f4db05e0040650a98
Kvakino <mikeknor2503@gmail.com>
2024-01-19 00:37:56 +0100

    completed day 23

--- src/day23/part2.rs
+++ src/day23/part2.rs
@@ -1,6 +1,9 @@
 use std::fs::File;
 use std::io::Read;
 
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 
 pub fn main(filename: &str) -> String {
     let mut file = File::open(filename).expect("Failed to open file");
@@ -21,44 +24,90 @@ pub fn main(filename: &str) -> String {
 
 fn get_longest_path(map: &Vec<Vec<char>>) -> usize {
     let finish_coords: (usize, usize) = (map.len()-1, map[0].len()-2);
-
-    let mut stack: Vec<((usize, usize), Vec<(usize, usize)>)> = vec![((1, 1), vec![(0, 1)])];
     let mut max_steps: usize = usize::MIN;
 
-    let (finish_coords, remaining_steps) = get_crossroad_coord(&map, (finish_coords.0-1, finish_coords.1), vec![finish_coords]);
+    let mut nodes: HashMap<(usize, usize), HashSet<((usize, usize), usize)>> = HashMap::new();
+
+    let (starting_coords, starting_remaining_steps) = get_crossroad_coord(&map, (1, 1), vec![(0, 1)]);
+    let (finish_coords, finish_remaining_steps) = get_crossroad_coord(&map, (finish_coords.0-1, finish_coords.1), vec![finish_coords]);
+
+    let mut stack: Vec<((usize, usize), (usize, usize), (usize, usize), usize)> = vec![(finish_coords, (0, 1), finish_coords, 0)];
 
     while !stack.is_empty() {
-        let (current_coord, mut history) = stack.pop().unwrap();
+        let (current_coord, last_coord, mut last_node, mut length) = stack.pop().unwrap();
 
-        if history.contains(&current_coord) {
+        if current_coord == (map.len()-1, map[0].len()-2) || current_coord == (1, 1) {
             continue;
         }
 
-        if current_coord == finish_coords {
-            if history.len() > max_steps {
-                max_steps = history.len()
-            }
-            println!("{}, {}", max_steps, stack.len());
-            continue;
-        }
-        
-        history.push(current_coord);
+        length += 1;
+
+        let mut n_of_paths: u8 = 0;
 
         if map[current_coord.0][current_coord.1+1] != '#' {
-            stack.push(((current_coord.0, current_coord.1+1), history.clone()))
+            n_of_paths += 1;
         }
         if map[current_coord.0+1][current_coord.1] != '#' {
-            stack.push(((current_coord.0+1, current_coord.1), history.clone()))
+            n_of_paths += 1;
         }
         if map[current_coord.0][current_coord.1-1] != '#' {
-            stack.push(((current_coord.0, current_coord.1-1), history.clone()))
+            n_of_paths += 1;
         }
         if map[current_coord.0-1][current_coord.1] != '#' {
-            stack.push(((current_coord.0-1, current_coord.1), history.clone()))
+            n_of_paths += 1;
+        }
+
+        if n_of_paths > 2 {
+            if nodes.keys().any(|key| *key == current_coord) {
+                nodes.entry(current_coord).or_insert(HashSet::from([(last_node, length)])).insert((last_node, length));
+                nodes.entry(last_node).or_insert(HashSet::from([(current_coord, length)])).insert((current_coord, length));
+
+                continue;
+            }
+            nodes.entry(current_coord).or_insert(HashSet::from([(last_node, length)])).insert((last_node, length));
+            nodes.entry(last_node).or_insert(HashSet::from([(current_coord, length)])).insert((current_coord, length));
+
+            last_node = current_coord;
+
+            length = 0;
+        }
+        
+        if map[current_coord.0][current_coord.1+1] != '#' && (current_coord.0, current_coord.1+1) != last_coord {
+            stack.push(((current_coord.0, current_coord.1+1), current_coord, last_node, length));
+        }
+        if map[current_coord.0+1][current_coord.1] != '#' && (current_coord.0+1, current_coord.1) != last_coord {
+            stack.push(((current_coord.0+1, current_coord.1), current_coord, last_node, length));
+        }
+        if map[current_coord.0][current_coord.1-1] != '#' && (current_coord.0, current_coord.1-1) != last_coord {
+            stack.push(((current_coord.0, current_coord.1-1), current_coord, last_node, length));
+        }
+        if map[current_coord.0-1][current_coord.1] != '#' && (current_coord.0-1, current_coord.1) != last_coord {
+            stack.push(((current_coord.0-1, current_coord.1), current_coord, last_node, length));
         }
     }
 
-    max_steps + remaining_steps
+    let mut stack: Vec<((usize, usize), HashSet<(usize, usize)>, usize)> = vec![((starting_coords), HashSet::new(), 0)];
+
+    while !stack.is_empty() {
+        let (current_node, mut visited_nodes, length) = stack.pop().unwrap();
+
+        if current_node == finish_coords {
+            if length > max_steps {
+                max_steps = length;
+                println!("{}", length+ starting_remaining_steps + finish_remaining_steps);
+            }
+        }
+
+        visited_nodes.insert(current_node);
+
+        for node in nodes.get(&current_node).unwrap() {
+            if !visited_nodes.contains(&node.0) {
+                stack.push((node.0, visited_nodes.clone(), length + node.1));
+            }
+        }
+    }
+
+    max_steps + starting_remaining_steps + finish_remaining_steps
 }
 
 
